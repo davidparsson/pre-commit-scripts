2df7ce24dfdf7700fe5ce4943091edc71bd6a111
David Pärsson <david.parsson@gmail.com>
2012-05-11 16:10:24 +0200

    Removed path duplication in MIGRATION_PATH and FILE_PATTERN, and clarified docs further

--- ordered-filename-pre-commit.py
+++ ordered-filename-pre-commit.py
@@ -5,26 +5,28 @@ import re
 import subprocess
 import optparse
 
-# Sub path to check. Will become root_module/<MIGRATION_PATH>, e.g. module/trunk/db/migrations/
+# Sub path to check. Final path will result in root_module/<MIGRATION_PATH>, 
+# e.g. mymodule/trunk/db/migrations/
 MIGRATION_PATH = "trunk/db/migrations/"
-# Incoming file pattern to check.
-FILE_PATTERN = "^.*/" + MIGRATION_PATH + "[0-9]+.*\.rb$"
-# Ignore this pre-commit check if the keyword is included in commit message
+# Incoming filename pattern in MIGRATION_PATH to check.
+# "[0-9].rb" would match mymodule/trunk/db/migrations/1.rb
+FILE_PATTERN = "[0-9]+.*\.rb$"
+# Ignores the pre-commit check if the keyword is included in the commit message
 SKIP_KEYWORD = "skip-migration-check"
 
 # Path to svnlook executable
 SVNLOOK_COMMAND = "svnlook"
 
-def check_filenames(file_pattern, skip_keyword, look_command):
+def check_filenames(skip_keyword, look_command):
   if should_skip_check_for_commit(skip_keyword, look_command):
     return 0
   error = 0
   added_files = get_files_added_in_commit(look_command)
   last_existing_file = None
   for added_file in added_files:
-    if should_check_file(file_pattern, added_file):
+    if should_check_file(added_file):
       if last_existing_file is None:
-        last_existing_filename = get_last_existing_matching_file(added_files, file_pattern, look_command)
+        last_existing_filename = get_last_existing_matching_file(added_files, look_command)
       if last_existing_filename and last_existing_filename > get_filename(added_file):
         sys.stderr.write("Error: The added file \"%s\" must have a filename \
 alphabetically after the existing \"%s\".\n" % (added_file, last_existing_filename))
@@ -39,8 +41,8 @@ def should_skip_check_for_commit(skip_keyword, look_command):
 def output_ignore_message(skip_keyword):
   sys.stderr.write("If you want to commit this anyway, include \"%s\" in the commit message.\n" % skip_keyword)
 
-def should_check_file(file_pattern, filename):
-  return bool(re.match(file_pattern, filename))
+def should_check_file(file_path):
+  return bool(re.match("^[^/]+/" + MIGRATION_PATH + FILE_PATTERN, file_path))
 
 def get_file_dir(filename):
   return filename[:filename.rfind("/") + 1]
@@ -55,19 +57,19 @@ def get_files_added_in_commit(look_command):
     return line[4:]
   return [filename(line) for line in get_changed_files(look_command) if added(line)]
 
-def get_last_existing_matching_file(added_files, file_pattern, look_command):
-  existing_matched_files = get_existing_matching_filenames(added_files, file_pattern, look_command)
+def get_last_existing_matching_file(added_files, look_command):
+  existing_matched_files = get_existing_matching_filenames(added_files, look_command)
   existing_matched_files.sort()
   if existing_matched_files:
     return existing_matched_files[-1]
   return ""
 
-def get_existing_matching_filenames(added_files, file_pattern, look_command):
+def get_existing_matching_filenames(added_files, look_command):
   root_dirs = get_files_in(".", look_command)
   all_filenames = []
   for root_dir in root_dirs:
     for file_path in get_files_in(root_dir + MIGRATION_PATH, look_command):
-      if should_check_file(file_pattern, file_path) and file_path not in added_files:
+      if should_check_file(file_path) and file_path not in added_files:
         all_filenames.append(get_filename(file_path))
   return all_filenames
 
@@ -104,7 +106,7 @@ matching files are added last, alphabetically."""
     (options, (repos, transaction_or_revision)) = parser.parse_args()
     look_option = ("--transaction", "--revision")[options.revision]
     look_command = "%s %s %s %s %s" % (SVNLOOK_COMMAND, "%s", repos, look_option, transaction_or_revision)
-    return check_filenames(FILE_PATTERN, SKIP_KEYWORD, look_command)
+    return check_filenames(SKIP_KEYWORD, look_command)
   except:
     parser.print_help()
     return 1
