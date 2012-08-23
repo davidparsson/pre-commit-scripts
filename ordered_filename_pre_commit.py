e9d2e5eaea207add933d9352e570efb5df5b3156
David Pärsson <david@parsson.se>
2012-08-23 11:06:23 +0200

    Whitespace: Indentation is now 4 spaces

--- ordered_filename_pre_commit.py
+++ ordered_filename_pre_commit.py
@@ -17,73 +17,73 @@ SKIP_KEYWORD = "skip-migration-check"
 SVNLOOK_COMMAND = "svnlook"
 
 def check_filenames(commit_details, repository_details):
-  if should_skip_check_for_commit(commit_details):
-    return 0
-  error = 0
-  last_existing_file = None
-  added_files = commit_details.get_added_files()
-  for added_file in added_files:
-    if should_check_file(added_file):
-      if last_existing_file is None:
-        last_existing_filename = get_last_existing_matching_file(added_files, repository_details)
-      if last_existing_filename and last_existing_filename > get_filename(added_file):
-        sys.stderr.write("Error: The added file \"%s\" must have a filename \
+    if should_skip_check_for_commit(commit_details):
+        return 0
+    error = 0
+    last_existing_file = None
+    added_files = commit_details.get_added_files()
+    for added_file in added_files:
+        if should_check_file(added_file):
+            if last_existing_file is None:
+                last_existing_filename = get_last_existing_matching_file(added_files, repository_details)
+            if last_existing_filename and last_existing_filename > get_filename(added_file):
+                sys.stderr.write("Error: The added file \"%s\" must have a filename \
 alphabetically after the existing \"%s\".\n" % (added_file, last_existing_filename))
-        error += 1
-  if error > 0:
-    output_ignore_message()
-  return error
+                error += 1
+    if error > 0:
+        output_ignore_message()
+    return error
 
 def should_skip_check_for_commit(commit_details):
-  return SKIP_KEYWORD in commit_details.get_commit_message().split()
+    return SKIP_KEYWORD in commit_details.get_commit_message().split()
 
 def output_ignore_message():
-  sys.stderr.write("If you want to commit this anyway, include \"%s\" in the commit message.\n" % SKIP_KEYWORD)
+    sys.stderr.write("If you want to commit this anyway, include \"%s\" in the commit message.\n" % SKIP_KEYWORD)
 
 def should_check_file(file_path):
-  return bool(re.match("^[^/]+/" + MIGRATION_PATH + FILE_PATTERN, file_path))
+    return bool(re.match("^[^/]+/" + MIGRATION_PATH + FILE_PATTERN, file_path))
 
 def get_file_dir(filename):
-  return filename[:filename.rfind("/") + 1]
+    return filename[:filename.rfind("/") + 1]
 
 def get_filename(filename):
-  return filename[filename.rfind("/") + 1:]
+    return filename[filename.rfind("/") + 1:]
 
 def get_last_existing_matching_file(added_files, repository_details):
-  existing_matched_files = get_existing_matching_filenames(added_files, repository_details)
-  existing_matched_files.sort()
-  if existing_matched_files:
-    return existing_matched_files[-1]
-  return ""
+    existing_matched_files = get_existing_matching_filenames(added_files, repository_details)
+    existing_matched_files.sort()
+    if existing_matched_files:
+        return existing_matched_files[-1]
+    return ""
 
 def get_existing_matching_filenames(added_files, repository_details):
-  root_dirs = repository_details.get_files_in(".")
-  all_filenames = []
-  for root_dir in root_dirs:
-    for file_path in repository_details.get_files_in(root_dir + MIGRATION_PATH):
-      if should_check_file(file_path) and file_path not in added_files:
-        all_filenames.append(get_filename(file_path))
-  return all_filenames
-  
+    root_dirs = repository_details.get_files_in(".")
+    all_filenames = []
+    for root_dir in root_dirs:
+        for file_path in repository_details.get_files_in(root_dir + MIGRATION_PATH):
+            if should_check_file(file_path) and file_path not in added_files:
+                all_filenames.append(get_filename(file_path))
+    return all_filenames
+    
 
 def main():
-  usage = """Usage: %prog REPOS TXN
+    usage = """Usage: %prog REPOS TXN
 
 Runs pre-commit verification on a repository transaction, verifying that
 matching files are added last, alphabetically."""
-  parser = optparse.OptionParser(usage=usage)
-  parser.add_option("-r", "--revision",
-                    help="Test mode. Specify a revision instead of a transaction.",
-                    action="store_true", default=False)
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-r", "--revision",
+                      help="Test mode. Specify a revision instead of a transaction.",
+                      action="store_true", default=False)
 
-  try:
-    (options, (repos, transaction_or_revision)) = parser.parse_args()
-    commit_details = CommitDetails(repos, transaction_or_revision, test_mode=options.revision)
-    repository_details = RepositoryDetails(repos, transaction_or_revision, test_mode=options.revision)
-    return check_filenames(commit_details, repository_details)
-  except:
-    parser.print_help()
-    return 1
+    try:
+        (options, (repos, transaction_or_revision)) = parser.parse_args()
+        commit_details = CommitDetails(repos, transaction_or_revision, test_mode=options.revision)
+        repository_details = RepositoryDetails(repos, transaction_or_revision, test_mode=options.revision)
+        return check_filenames(commit_details, repository_details)
+    except:
+        parser.print_help()
+        return 1
 
 if __name__ == "__main__":
-  sys.exit(main())
+    sys.exit(main())
